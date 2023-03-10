cd8067e429b88aec4f9b04a6fd6f114a010b5500
devoprohan <rohanvermadev@gmail.com>
2023-03-10 03:26:16 -0500

    Store the file in memory

--- index.js
+++ index.js
@@ -8,15 +8,7 @@ const app = express();
 
 app.use(cors()); // enable CORS for all routes
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, 'uploads'));
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    cb(null, uniqueSuffix + '-' + file.originalname);
-  }
-});
+const storage = multer.memoryStorage();
 
 const upload = multer({ storage: storage });
 
@@ -28,7 +20,6 @@ app.post('/upload', upload.single('file'), (req, res) => {
   //returning session
   console.log(`Sending Session ${sessionId}`);
   res.json({ sessionId: sessionId });
-  
 });
 
 const port = process.env.PORT || 3000;
