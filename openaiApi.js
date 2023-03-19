c04c7f999e97def2cdb4b81e5ed85f8780a5d4bd
Rohan Verma <86923317+DevOpRohan@users.noreply.github.com>
2023-03-19 19:53:14 +0000

    Refactored

--- openaiApi.js
+++ openaiApi.js
@@ -1,25 +1,3 @@
-// const { Configuration, OpenAIApi } = require('openai');
-
-// const configuration = new Configuration({
-//   apiKey: process.env.OPENAI_API_KEY,
-// });
-
-// const openai = new OpenAIApi(configuration);
-
-// async function createEmbeddings(model, input) {
-//   try {
-//     console.log(`Creating embeddings for ${input.length} inputs...`)
-//     const response = await openai.createEmbedding({ model, input });
-//     return response.data.data.map((data) => data.embedding);
-//   } catch (error) {
-//     console.error(`Error: ${error.message}`);
-//     throw error;
-//   }
-// }
-
-// module.exports = {
-//   createEmbeddings,
-// };
 const { Configuration, OpenAIApi } = require('openai');
 
 const configuration = new Configuration({
